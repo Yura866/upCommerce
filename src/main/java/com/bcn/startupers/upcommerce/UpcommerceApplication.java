2480bcfae427243a14e940f8be1dca471f885224
yhuzo <yhuzo@opentrends.net>
2020-09-11 16:44:24 +0200

    initial commit

---
+++ src/main/java/com/bcn/startupers/upcommerce/UpcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.bcn.startupers.upcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcn/startupers/upcommerce/UpcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcn.startupers.upcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
